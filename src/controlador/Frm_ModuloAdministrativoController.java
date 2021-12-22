3c180a1e0fca3e190c00e55b4ea162751e1b21dd
JainerPinta <jainer.pinta@unl.edu.ec>
2021-12-22 02:43:35 -0500

    Añadir funcionalidad a botones

--- src/controlador/Frm_ModuloAdministrativoController.java
+++ src/controlador/Frm_ModuloAdministrativoController.java
@@ -5,9 +5,18 @@
  */
 package controlador;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +24,37 @@ import javafx.fxml.Initializable;
  * @author Jainer Pinta
  */
 public class Frm_ModuloAdministrativoController implements Initializable {
-
+    private @FXML Button btnAdd;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    private void seleccionarVentana(ActionEvent e){
+        if (e.getSource() == btnAdd) {
+            cargarVentana("/vista/Frm_PanelAddEmpleado.fxml");
+        }else{
+            cargarVentana("/vista/Frm_AsignarCargo.fxml");
+        }
+    }
+    
+    private void cargarVentana(String direccion){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource(direccion));
+            Scene escena1 = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(escena1);
+            stage.setTitle("Añadir Empleado");
+            stage.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        } 
+    }
+    
+    
     
 }
