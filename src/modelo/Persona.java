07581a9ad8bd152a1f393aa8d06981f108ce6a2e
JainerPinta <jainer.pinta@unl.edu.ec>
2021-12-21 20:21:48 -0500

    Añadir atributos faltantes a la clase Persona

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -11,17 +11,43 @@ package modelo;
  */
 public class Persona {
     private Long idPersona;
-    private String nombre;
+    private String nombres;
+    private String apellidos;
     private String telefono;
     private String direccion;
     private String cedula;
+    private int edad;
 
-    public String getNombre() {
-        return nombre;
+    public Long getIdPersona() {
+        return idPersona;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setIdPersona(Long idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     public String getTelefono() {
