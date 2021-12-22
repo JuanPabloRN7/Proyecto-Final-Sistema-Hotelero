86ae0815151eb39869bcdf7356e7f21aa90ffbf5
JainerPinta <jainer.pinta@unl.edu.ec>
2021-12-21 21:13:15 -0500

    Implementar algoritmo para verificar cedula

---
+++ src/controlador/PersonaController.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import modelo.Persona;
+
+/**
+ *
+ * @author Jainer Pinta
+ */
+public class PersonaController {
+    private Persona persona;
+
+    public Persona getPersona() {
+        if(this.persona == null)
+            this.persona = new Persona();
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+    
+    public boolean verificarCedula(){
+        String cedula = persona.getCedula();
+        int sumaTotal = 0;
+        for (int i = 0; i < cedula.length()-1; i++) {
+            if ((i+1)%2 == 0) {
+                sumaTotal += Character.getNumericValue(cedula.charAt(i));
+            }else{
+                int num = Character.getNumericValue(cedula.charAt(i))*2;
+                if (num > 9) {
+                    num = num-9;
+                }           
+                sumaTotal += num;
+            }
+        }
+        if (10-(sumaTotal%10) == Character.getNumericValue(cedula.charAt(cedula.length()-1))) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/proyecto/pkgfinal/sistema/hotelero/ProyectoFinalSistemaHotelero.java
+++ src/proyecto/pkgfinal/sistema/hotelero/ProyectoFinalSistemaHotelero.java
@@ -5,17 +5,24 @@
  */
 package proyecto.pkgfinal.sistema.hotelero;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Jainer Pinta
  */
-public class ProyectoFinalSistemaHotelero {
-
+public class ProyectoFinalSistemaHotelero extends Application{
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
     }
     
 }
