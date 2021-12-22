967f918a12c9a113270ec71d35c924190428ec88
JainerPinta <jainer.pinta@unl.edu.ec>
2021-12-22 02:46:34 -0500

    Implementar la ventana principal

--- src/proyecto/pkgfinal/sistema/hotelero/ProyectoFinalSistemaHotelero.java
+++ src/proyecto/pkgfinal/sistema/hotelero/ProyectoFinalSistemaHotelero.java
@@ -6,6 +6,9 @@
 package proyecto.pkgfinal.sistema.hotelero;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
@@ -22,7 +25,15 @@ public class ProyectoFinalSistemaHotelero extends Application{
     
     @Override
     public void start(Stage stage) throws Exception {
-        
+         try{
+            Parent root = FXMLLoader.load(getClass().getResource("/vista/Frm_VentanaGeneral.fxml"));
+            Scene escena1 = new Scene(root);
+            stage.setScene(escena1);
+            stage.setTitle("Sistema Hotelero");
+            stage.show();
+        } catch(Exception e){
+            e.printStackTrace();
+        }        
     }
     
 }
