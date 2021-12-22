060f951d00f4acf143d58b9b26f78f5eea1f7687
JainerPinta <jainer.pinta@unl.edu.ec>
2021-12-22 02:43:03 -0500

    Añadir campos extra al formulario

--- src/controlador/Frm_PanelAddEmpleadoController.java
+++ src/controlador/Frm_PanelAddEmpleadoController.java
@@ -5,10 +5,15 @@
  */
 package controlador;
 
+import Controlador.ConexionDB;
+import controlador.daos.EmpleadoDao;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -19,11 +24,16 @@ import javafx.scene.layout.VBox;
  * @author Jainer Pinta
  */
 public class Frm_PanelAddEmpleadoController implements Initializable {
+    private ConexionDB conexionDB = new ConexionDB();
+    private EmpleadoDao empleadoDao = new EmpleadoDao();
+    private PersonaController pc = new PersonaController();
+    
     private @FXML TextField txtNombres;
     private @FXML TextField txtApellidos;
     private @FXML TextField txtTelefono;
     private @FXML TextField txtDireccion;
     private @FXML TextField txtCedula;
+    private @FXML TextField txtID;
     private @FXML DatePicker txtFecha;
     private @FXML VBox vboxDatos;
     /**
@@ -32,6 +42,57 @@ public class Frm_PanelAddEmpleadoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
+    @FXML
+    private void addEmpleado(){
+        if (verificarCampos()) {
+            empleadoDao.getEmpleado().setApellidos(txtApellidos.getText());
+            empleadoDao.getEmpleado().setNombres(txtNombres.getText());
+            empleadoDao.getEmpleado().setTelefono(txtTelefono.getText());
+            empleadoDao.getEmpleado().setDireccion(txtDireccion.getText());
+            empleadoDao.getEmpleado().setCedula(txtCedula.getText());
+            empleadoDao.getEmpleado().setFechaNacimiento(txtFecha.getValue());
+            empleadoDao.getEmpleado().setIdentificacion(txtID.getText());
+            empleadoDao.getEmpleado().setCargo("");
+            pc.setPersona(empleadoDao.getEmpleado());
+            if (pc.verificarCedula()) {
+                if (empleadoDao.guardar()) {
+                    crearAlerta(AlertType.INFORMATION, "OK", "Datos guardados", "Los datos se han almacenado correctamente");
+                }else{
+                    crearAlerta(AlertType.ERROR, "Error", "Datos no guardados", "Ha surgido un error al guardar los datos");
+                }
+            }else{
+                crearAlerta(AlertType.ERROR, "Error", "Cedula inválida", "El número de cedula ingresado no es válido");
+            }
+        }else{
+            crearAlerta(AlertType.INFORMATION, "Error", "Campos vacios", "Rellene todos los campos para guardar al empleado");
+        }
+    }
+     
+    private void crearAlerta(Alert.AlertType tipo, String titulo, String cabecera, String mensaje){
+        Alert alerta = new Alert(tipo);
+        alerta.setTitle(titulo);
+        alerta.setHeaderText(cabecera);
+        alerta.setContentText(mensaje);
+        alerta.showAndWait();
+    }
+    
+    private boolean verificarCampos(){
+        boolean camposTexto = true;
+        for (Node nodo : vboxDatos.getChildren()) {
+            if (nodo instanceof TextField) {
+                if (((TextField)nodo).getText().trim().isEmpty()) {
+                    camposTexto = false;
+                }
+            }
+        }
+        if (camposTexto && txtFecha.getValue() != null) {
+            System.out.println("Paso");
+            return true;
+        }
+        System.out.println("No paso");
+        return false;
+    }
     
 }
