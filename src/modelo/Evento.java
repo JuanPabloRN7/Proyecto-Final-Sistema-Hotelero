ae516f1ecd4b8919445ddafc1a8335ab0386b1ce
Juan Pablo <Usuario@LAPTOP-ARP1MDVA>
2021-12-23 20:54:26 -0500

    Se agrego la clase Evento

---
+++ src/modelo/Evento.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Evento extends Servicio{
+    private String fecha;
+    private String hora;
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    
+}
