74371bacaa24b51ccb52b1a33ed3e2a2b295c5c4
dog-samy <jonathan.sarabia@unl.edu.ec>
2021-12-29 20:37:51 -0500

    se agrego la clse cliente

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.time.LocalDate;
+import modelo.Persona;
+
+/**
+ *
+ * @author jona-samy
+ */
+public class Cliente  extends Persona{
+    
+    
+    private String hora_entrada;
+    private String hora_estimada;
+    private String nrohabitacion;
+    private LocalDate Fecha_inicio;
+    private LocalDate Fecha_final;
+
+    public LocalDate getFecha_inicio() {
+        return Fecha_inicio;
+    }
+
+    public void setFecha_inicio(LocalDate Fecha_inicio) {
+        this.Fecha_inicio = Fecha_inicio;
+    }
+
+    public LocalDate getFecha_final() {
+        return Fecha_final;
+    }
+
+    public void setFecha_final(LocalDate Fecha_final) {
+        this.Fecha_final = Fecha_final;
+    }
+    
+    public String getNrohabitacion() {
+        return nrohabitacion;
+    }
+
+    public void setNrohabitacion(String nrohabitacion) {
+        this.nrohabitacion = nrohabitacion;
+    }
+
+    public String getHora_entrada() {
+        return hora_entrada;
+    }
+
+    public void setHora_entrada(String hora_entrada) {
+        this.hora_entrada = hora_entrada;
+    }
+
+    public String getHora_estimada() {
+        return hora_estimada;
+    }
+
+    public void setHora_estimada(String hora_estimada) {
+        this.hora_estimada = hora_estimada;
+    }
+    
+
+    
+}
