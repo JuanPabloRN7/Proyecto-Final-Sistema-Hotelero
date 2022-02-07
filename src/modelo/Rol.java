143a8c83ff7a87fd0074652416ade59e4d644263
JainerPinta <jainer.pinta@unl.edu.ec>
2022-02-07 12:18:30 -0500

    Añadir comentarios

--- src/modelo/Rol.java
+++ src/modelo/Rol.java
@@ -12,23 +12,43 @@ package modelo;
 public class Rol {
     private String cargo;
     private boolean autorizacion;
-
+    
+    /**
+     * Obtnener el booleano autorizacion de la clase Rol.
+     * @return Retorna true si esta autorizado.
+     */
     public boolean isAutorizacion() {
         return autorizacion;
     }
-
+    
+    /**
+     * Setear el valor de autorizacion.
+     * @param autorizacion Boolean valor de autorizacion.
+     */
     public void setAutorizacion(boolean autorizacion) {
         this.autorizacion = autorizacion;
     }
 
+    /**
+     * Obtener cargo del empleado.
+     * @return String cargo del empleado.
+     */
     public String getCargo() {
         return cargo;
     }
-
+    
+    /**
+     * Setear cargo del empleado
+     * @param cargo String cargo del empleado.
+     */
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
-
+    
+    /**
+     * Valor string de la clase rol.
+     * @return Retorna el cargo del empleado.
+     */
     @Override
     public String toString() {
         return this.cargo;
