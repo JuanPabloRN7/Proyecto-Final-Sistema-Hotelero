1dc65d78518b870c0cf6d86bd9f0c20602706c0a
Juan Pablo <Usuario@LAPTOP-ARP1MDVA>
2022-02-07 19:49:28 -0500

    .

--- src/controlador/PersonaController.java
+++ src/controlador/PersonaController.java
@@ -15,30 +15,31 @@ public class PersonaController {
     private Persona persona;
 
     public Persona getPersona() {
-        if(this.persona == null)
+        if (this.persona == null) {
             this.persona = new Persona();
+        }
         return persona;
     }
 
     public void setPersona(Persona persona) {
         this.persona = persona;
     }
-    
-    public boolean verificarCedula(){
+
+    public boolean verificarCedula() {
         String cedula = persona.getCedula();
         int sumaTotal = 0;
-        for (int i = 0; i < cedula.length()-1; i++) {
-            if ((i+1)%2 == 0) {
+        for (int i = 0; i < cedula.length() - 1; i++) {
+            if ((i + 1) % 2 == 0) {
                 sumaTotal += Character.getNumericValue(cedula.charAt(i));
-            }else{
-                int num = Character.getNumericValue(cedula.charAt(i))*2;
+            } else {
+                int num = Character.getNumericValue(cedula.charAt(i)) * 2;
                 if (num > 9) {
-                    num = num-9;
-                }           
+                    num = num - 9;
+                }
                 sumaTotal += num;
             }
         }
-        if (10-(sumaTotal%10) == Character.getNumericValue(cedula.charAt(cedula.length()-1))) {
+        if (10 - (sumaTotal % 10) == Character.getNumericValue(cedula.charAt(cedula.length() - 1))) {
             return true;
         }
         return false;
