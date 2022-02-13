3498d02bd9fa8443938003093727daf41dfb93d3
samy <samy@hp>
2022-02-13 13:52:36 -0500

    le cambie el modelo persona por cliente

--- src/controlador/daos/AdaptadorDaoCliente.java
+++ src/controlador/daos/AdaptadorDaoCliente.java
@@ -11,7 +11,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import lista.controlador.Lista;
+import modelo.Cliente;
 import modelo.Persona;
 
 /**
@@ -41,6 +43,34 @@ public class AdaptadorDaoCliente<T> implements InterfazDao<T> {
             ps.setString(3, persona.getCedula());
             ps.setString(4, persona.getDireccion());
             ps.setString(5, persona.getTelefono());
+            int verificacion = ps.executeUpdate();
+            ps.close();
+            if (verificacion > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+     public boolean guardar1(T dato) {
+        Cliente persona = (Cliente) dato;
+        Connection conexion = conexionDB.conectar();
+        try {
+            PreparedStatement ps = conexion.prepareStatement("INSERT INTO clientes(Nombres,Apellidos,Cedula,Direccion,Telefono,Habitacion,Hora_Entrada,Fecha_Salida) VALUE(?,?,?,?,?,?,?,?)");
+            //ps.setLong(1, persona.getIdPersona());
+            ps.setString(1, persona.getNombres());
+            ps.setString(2, persona.getApellidos());
+            ps.setString(3, persona.getCedula());
+            ps.setString(4, persona.getDireccion());
+            ps.setString(5, persona.getTelefono());
+            ps.setString(6, persona.getNrohabitacion());
+            ps.setString(7, persona.getHora_entrada());
+            ps.setString(8, persona.getFecha_final().toString());
+            
             int verificacion = ps.executeUpdate();
             ps.close();
             if (verificacion > 0) {
@@ -83,9 +113,40 @@ public class AdaptadorDaoCliente<T> implements InterfazDao<T> {
         }
         return lista;
     }
+     public Lista<T> listar1() {
+        Statement st = null;
+        ResultSet rs = null;
+        lista = new Lista<>();
+        Connection conexion = conexionDB.conectar();  
+        try {
+            st = conexion.createStatement();
+            rs = st.executeQuery("SELECT * FROM clientes");
+            while (rs.next()) {    
+                Cliente cliente = new Cliente();
+                cliente.setIdPersona(rs.getLong("IdCliente"));
+                cliente.setNombres(rs.getString("Nombres"));
+                cliente.setApellidos(rs.getString("Apellidos"));
+                cliente.setCedula(rs.getString("Cedula"));
+                cliente.setDireccion(rs.getString("Direccion"));
+                cliente.setTelefono(rs.getString("Telefono"));
+                cliente.setNrohabitacion(rs.getString("Habitacion"));
+                cliente.setHora_entrada(rs.getString("Hora_Entrada"));
+                cliente.setFecha_final(LocalDate.parse(rs.getString("Fecha_Salida")));
+                lista.insertarNodo((T) cliente);
+            }            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return lista;
+    }
 
     @Override
     public boolean modificar(T dato) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modificar(String dato, String ID) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
